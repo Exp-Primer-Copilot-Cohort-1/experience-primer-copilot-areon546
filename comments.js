bc6d68b7dfc1d7d307c4495f43f988a587a065f3
areon546 <57325743+areon546@users.noreply.github.com>
2024-01-30 08:33:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,56 +1,25 @@
-// create web server
-// 1. load module
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
-var template = require('./lib/template.js');
+// Create web server
+// Run: node comments.js
+// Then, open browser and go to: http://localhost:3000/
 
-// 2. create server
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query;
-    var pathname = url.parse(_url,true).pathname;
-    // console.log(url.parse(_url,true));
-    // console.log(pathname);
-    // console.log(queryData);
-    // console.log(queryData.id);
-    // console.log(_url);
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-    if(pathname === '/'){
-        if(queryData.id === undefined){
-            fs.readdir('./data',function(error,filelist){
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = template.list(filelist);
-                var html = template.HTML(title,list,`<h2>${title}</h2>${description}`,
-                `<a href="/create">create</a>`);
-                response.writeHead(200);
-                response.end(html);
-            });
-        }else{
-            fs.readdir('./data',function(error,filelist){
-                var filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`,'utf8',function(err,description){
-                    var title = queryData.id;
-                    var list = template.list(filelist);
-                    var html = template.HTML(title,list,`<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>
-                    <a href="/update?id=${title}">update</a>
-                    <form action="delete_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
-                        <input type="submit" value="delete">
-                    </form>`);
-                    response.writeHead(200);
-                    response.end(html);
-                });
-            });
-        }
-    }else if(pathname === '/create'){
-        fs.readdir('./data',function(error,filelist){
-            var title = 'WEB - create';
-            var list = template.list(filelist);
-            var html = template.HTML(title,list,`
-            <form action="/create_process" method="post">
-                <p><input type="text" name="title" placeholder="title"></p>
-                <p><textarea name="description" placeholder="description"></textarea></p>
+// Create application
+const app = express();
+const port = 3000;
+
+// Set up middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up routes
+app.use(require('./routes'));
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
